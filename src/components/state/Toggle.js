d37949d95f3474afcb7314f2d17d95a8a6e1acaa
HQ Nam <quangnamhd9x@gmail.com>
2022-01-21 17:45:26 +0700

    togglt add onclick on spinner

--- src/components/state/Toggle.js
+++ src/components/state/Toggle.js
@@ -4,19 +4,28 @@ import "./ToggleStyles.css";
 function Toggle() {
   const [on, setOn] = useState(false);
   console.log(on);
+  const handleToggle = () => {
+      // setOn(CallBack) -> setOn(prevState => !prevState)
+      setOn((on) => !on)
+    // =
+    //   setOn((on) => {
+    //       console.log(on);
+    //       return !on;
+    //   });
+  }
   return (
     <div>
-      <div className={`toggle ${on ? "active" : ""}`}>
+      <div className={`toggle ${on ? "active" : ""}`} onClick={handleToggle}>
         <div className={`spinner ${on ? "active" : ""}`}></div>
       </div>
-      <div className="toggle-control">
+      {/* <div className="toggle-control">
         <div className="toggle-on" onClick={() => setOn(true)}>
           ON
         </div>
         <div className="toogle-off" onClick={() => setOn(false)}>
           OFF
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
