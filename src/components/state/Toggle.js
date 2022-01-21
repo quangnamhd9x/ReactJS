0453e195fbcd048e752df00a57b43be9b9a8efde
HQ Nam <quangnamhd9x@gmail.com>
2022-01-21 17:10:44 +0700

    add style toggle demo

--- src/components/state/Toggle.js
+++ src/components/state/Toggle.js
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
+import "./ToggleStyles.css";
 
 function Toggle() {
-  // 1. enableing state: useState(initialize value)
-  // 2. initialize state: useState(false)
-  // 3. reading state:
-  // 4. update state:
-  // const array = useState(false);
-  // console.log(array);
-  // console.log(array[0]);
-  // console.log(array[1]);
-  // const [a, b] = [1,2];
-  // console.log(a, b);
-  // initialize value: boolean(true, false), number (1,2,3,4), string("namhq"), undefined, null, [1,2,3,4], {title: "frontend"}
-  // state changes -> re-render
   const [on, setOn] = useState(false);
-  console.log(on, setOn);
   console.log(on);
   return (
-    <div className="toggle" onClick={() => setOn(true)}>
-      Toggle {on ? "on" : "off"}
+    <div>
+      <div className={`toggle ${on ? "active" : ""}`}>
+        <div className={`spinner ${on ? "active" : ""}`}></div>
+      </div>
+      <div className="toggle-control">
+        <div className="toggle-on" onClick={() => setOn(true)}>
+          ON
+        </div>
+        <div className="toogle-off" onClick={() => setOn(false)}>
+          OFF
+        </div>
+      </div>
     </div>
   );
 }
