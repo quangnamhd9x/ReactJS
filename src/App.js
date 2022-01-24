1dcdd38ac646687e44047b0a8a825c384a20fdfe
HQ Nam <quangnamhd9x@gmail.com>
2022-01-24 16:52:26 +0700

    add demo stale state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import Toggle from "./components/state/Toggle";
+import Counter from "./components/counter/Counter";
+import Toggle from "./components/toogle/Toggle";
 function App() {
   return (
     <div>
       <Toggle></Toggle>
+      <Counter></Counter>
     </div>
   );
 }
