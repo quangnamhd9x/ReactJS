af0b9740794c478cc8093d295b209d5718a2eff5
HQ Nam <quangnamhd9x@gmail.com>
2022-01-25 09:15:37 +0700

    tictactoe part 2

--- src/components/tictactoe/Board.js
+++ src/components/tictactoe/Board.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { calulateWinner } from "../../helper";
 import Cell from "./Cell";
 
 const Board = () => {
+  const cells = [null, null, null, "X", "X", "X", null, null, null];
+  console.log(calulateWinner(cells));
   return (
     <div className="game-board">
       {Array(9)
         .fill()
         .map((item, index) => (
-          <Cell key={index}></Cell>     // tạo ra nhiều phần tử giống nhau
+          <Cell key={index}></Cell> // tạo ra nhiều phần tử giống nhau
         ))}
     </div>
   );
