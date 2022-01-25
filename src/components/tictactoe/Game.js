9b25f5ac7c0d8837db516eeb8ed1f088fbe03be0
HQ Nam <quangnamhd9x@gmail.com>
2022-01-25 16:51:17 +0700

    b31c3 game tictactoe part3 add logic check winner

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -7,10 +7,23 @@ const Game = () => {
     const [board, setBoard] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
     const winner = calulateWinner(board);
-    const handleClick = () => {}
+    const handleClick = (index) => {
+        const boardCopy = [...board];
+        if(winner || boardCopy[index]) return;
+        boardCopy[index] = xIsNext ? 'X' : 'O';
+        setBoard(boardCopy);
+        setXIsNext(!xIsNext);
+    }
+
+    const handleResetGame = () => {
+        setBoard(Array(9).fill(null));   // if you want orther player win start game 
+        //setXIsNext(true);    // if you want set alaway first X
+    }
+
     return (
         <div>
             <Board cells={board} onClick={handleClick}></Board>
+            <button onClick={handleResetGame}>ResetGame</button>
         </div>
     );
 };
