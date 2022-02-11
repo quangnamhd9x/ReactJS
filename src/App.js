5fe954010dd4fdcbcd1b6c2d26a51057796b8c43
HQ Nam <quangnamhd9x@gmail.com>
2022-02-11 17:05:34 +0700

    refactor card component using tailwind css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { ThemeProvider } from "styled-components";
+// import { ThemeProvider } from "styled-components";
 import "./App.css";
-import Card2 from "./components/card/Card2";
+// import Card2 from "./components/card/Card2";
 import CardList from "./components/card/CardList";
-import { GlobalStyles } from "./GlobalStyles";
+import CardTailwind from "./components/card/CardTailwind";
+// import { GlobalStyles } from "./GlobalStyles";
 
-const theme = {
-  colors: {
-    blue: "#2979ff",
-  },
-  orange: "#ffa400",
-};
+// const theme = {
+//   colors: {
+//     blue: "#2979ff",
+//   },
+//   orange: "#ffa400",
+// };
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <GlobalStyles></GlobalStyles>
+    <div>
+      {/* <ThemeProvider theme={theme}>
+      </ThemeProvider> */}
+
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      {/* <GlobalStyles></GlobalStyles> */}
       <CardList>
-        <Card2 secondary={true}></Card2>
+        <CardTailwind></CardTailwind>
       </CardList>
-    </ThemeProvider>
+    </div>
   );
 }
 
---
+++ src/components/card/CardTailwind.js
@@ -0,0 +1,36 @@
+import React from "react";
+import img from "./img/dr1.png";
+
+const CardTailwind = () => {
+  return (
+    <div className="relative">
+      <div className="w-full rounded-lg h-[400px]">
+        <img src={img} alt="" />
+      </div>
+      <div className="absolute left-2/4 -translate-x-2/4 bottom-0 bg-white z-10 rounded-[20px] p-5 w-[calc(100%-36px)]">
+        <div className="flex justify-between items-center mb-8">
+          <div className="flex items-center gap-x-3">
+            <img
+              className="w-8 h-8 rounded-full object-cover flex-shink-0"
+              src={img}
+              alt=""
+            /> 
+            <span className="font-light text-base text-[#333]">@zndrson</span>
+          </div>
+          <div className="flex items-center gap-x-3">
+            <img src="/icon-heart.svg" alt="" />
+            <span>256</span>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <h3 className="text-lg font-medium">Cosmic Perspective</h3>
+          <span className="text-lg font-bold text-transparent bg-clip-text" fontSize="22px">
+            12,000 PSL
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardTailwind;
