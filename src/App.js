9aa1b71d1c42620c48c4d0cd271d15956053f041
HQ Nam <quangnamhd9x@gmail.com>
2022-02-14 13:57:53 +0700

    add paging page photo with useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import "./components/youtube/yt-style.scss"
-import YoutubeSideBar from "./components/youtube/YoutubeSideBar";
-import YoutubeList from "./components/youtube/YoutubeList";
+import Photo from "./components/photo/Photo";
 
 function App() {
   return (
-    <div className="youtube-main">
-      <YoutubeSideBar></YoutubeSideBar>
-      <YoutubeList></YoutubeList>
+    <div>
+      <Photo></Photo>
     </div>
   );
 }
--- src/components/photo/Photo.js
+++ src/components/photo/Photo.js
@@ -1,10 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const getRandomPhotos = (page) => {
+  return axios
+    .get(`https://picsum.photos/v2/list?page=${page}&limit=8`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 const Photo = () => {
+  const [randomPhotos, setRandomPhotos] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+
+  const handleLoadMotePhotos = () => {
+    getRandomPhotos(nextPage).then((images) => {
+      const newPhotos = [...randomPhotos, ...images];
+      console.log(images);
+      setRandomPhotos(newPhotos);
+      setNextPage(nextPage + 1);
+    });
+  };
   useEffect(() => {
-    // side effect
+    handleLoadMotePhotos();
   }, []);
-  return <div>photo</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-5 p-5">
+        {randomPhotos.length > 0 &&
+          randomPhotos.map((item, index) => (
+            <div key={item.id} className="p-3 bg-white shadow-md rounded-lg">
+              <img
+                src={item.download_url}
+                alt={item.author}
+                className="w-full h-full object-cover rounded-lg h-[200px]"
+              />
+            </div>
+          ))}
+      </div>
+      <div className="text-center">
+        <button
+          onClick={handleLoadMotePhotos}
+          className="inline-block px-8 py-4 bg-purple-600 text-white"
+        >
+          Load More
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Photo;
